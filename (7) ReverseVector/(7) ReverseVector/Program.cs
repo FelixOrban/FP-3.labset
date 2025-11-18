04484c10880d699c7f5d103ca9676bc3fa10d59d
FelixOrban <felixorban123@gmail.com>
2025-11-18 16:43:04 +0200

    a

---
+++ (7) ReverseVector/(7) ReverseVector/Program.cs
@@ -0,0 +1,24 @@
+﻿//Reverse. Se da un vector nu n elemente. Se cere sa se inverseze ordinea elementelor din vector. Prin inversare se intelege ca primul element devine ultimul, al doilea devine penultimul etc.
+
+namespace ReverseVector;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Vector = ");
+        int[] vector = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int[] vector2 = new int[n];
+
+        for(int i = 0; i<n; ++i)
+            vector2[i] = vector[n - 1 - i];
+
+        Console.WriteLine(string.Join(' ' , vector2));
+    }
+}
