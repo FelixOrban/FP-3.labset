37b8dacf9c0b81f770bb73673f2e1c38f9bc0ecb
FelixOrban <felixorban123@gmail.com>
2025-11-18 19:15:14 +0200

    a

---
+++ (11) Eratostene/(11) Eratostene/Program.cs
@@ -0,0 +1,34 @@
+﻿//Se da un numar natural n. Se cere sa se afiseze toate numerele prime mai mici sau egale cu n (ciurul lui Eratostene). 
+
+namespace Eratostene;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        bool[] isPrime = new bool[n + 1];
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for (int i = 2; i < isPrime.Length; ++i)
+            isPrime[i] = true;
+
+        for (int i = 2; i * i <= n; ++i)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i * i; j <= n; j += i)
+                    isPrime[j] = false;
+            }
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (isPrime[i])
+                Console.Write(i + " ");
+        }
+    }
+}
