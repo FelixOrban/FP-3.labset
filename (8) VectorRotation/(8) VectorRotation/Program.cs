679403bb5cc100342d3c08cc838ac5e02a637c09
FelixOrban <felixorban123@gmail.com>
2025-11-18 18:28:03 +0200

    a

---
+++ (8) VectorRotation/(8) VectorRotation/Program.cs
@@ -0,0 +1,26 @@
+﻿//Rotire. Se da un vector cu n elemente. Rotiti elementele vectorului cu o pozitie spre stanga. Prin rotire spre stanga primul element devine ultimul, al doilea devine primul etc. 
+
+namespace VectorRotation;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Vector = ");
+        int[] vector = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+        int[] vector2 = new int[n];
+
+        for (int i = 0; i < n - 1; ++i)
+            vector2[i] = vector[i + 1];
+        vector2[n - 1] = vector[0];
+
+        Console.WriteLine(string.Join(' ', vector2));
+
+    }
+}
