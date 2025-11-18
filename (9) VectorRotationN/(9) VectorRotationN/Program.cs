9fc496025166fc4ea121bd102440d1a9d88960e6
FelixOrban <felixorban123@gmail.com>
2025-11-18 18:42:26 +0200

    a

---
+++ (9) VectorRotationN/(9) VectorRotationN/Program.cs
@@ -0,0 +1,28 @@
+﻿//Rotire k. Se da un vector cu n elemente. Rotiti elementele vectorului cu k pozitii spre stanga. 
+
+namespace VectorRotation;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Vector = ");
+        int[] vector = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+        Console.Write("k = ");
+        int k = int.Parse(Console.ReadLine()!);
+
+        int[] vector2 = new int[n];
+
+        for (int i = 0; i < n; ++i)
+            vector2[i] = vector[(i + k) % n] ;
+
+        Console.WriteLine(string.Join(' ', vector2));
+
+    }
+}
