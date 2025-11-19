3dfebed62228879630c0818fe7e837cff6ed6390
FelixOrban <felixorban123@gmail.com>
2025-11-19 20:02:20 +0200

    a

---
+++ (18) PolinomHorner/(18) PolinomHorner/Program.cs
@@ -0,0 +1,27 @@
+﻿//Se da un polinom de grad n ai carui coeficienti sunt stocati intr-un vector. Cel mai putin semnificativ coeficient este pe pozitia zero in vector. Se cere valoarea polinomului intr-un punct x. 
+
+namespace PolinomHorner;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Vector = ");
+        int[] v = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+        Console.Write("x = ");
+        int x = int.Parse(Console.ReadLine()!);
+
+        int result = v[n];
+
+        for (int i = n-1; i >= 0; --i)
+            result = result * x + v[i]; //Horner, P(x)=((an​)x+an−1​)x+an−2​⋯+a0​
+
+        Console.WriteLine($"{result}");
+    }
+}
