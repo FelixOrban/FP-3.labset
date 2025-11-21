2e792c193f3b20873ab4637da19ed87cef3551fb
FelixOrban <felixorban123@gmail.com>
2025-11-21 16:37:15 +0200

    a

---
+++ (26) HugeNumbers/(26) HugeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿//Se dau doua numere naturale foarte mari (cifrele unui numar foarte mare sunt stocate intr-un vector - fiecare cifra pe cate o pozitie). Se cere sa se determine suma, diferenta si produsul a doua astfel de numere. 
+
+namespace HugeNumbers;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("First number = ");
+        string input1 = Console.ReadLine()!;
+        int[] v1 = input1.Select(x => x - '0').ToArray(); 
+        int n = v1.Length;
+
+        Console.Write("Second number = ");
+        string input2 = Console.ReadLine()!;
+        int[] v2 = input2.Select(y=> y - '0').ToArray();
+        int m = v2.Length;
+
+        
+
+    }
+}
---
+++ (27) SortedIndex/(27) SortedIndex/Program.cs
@@ -0,0 +1,36 @@
+﻿// Se da un vector si un index in vectorul respectiv. Se cere sa se determine valoarea din vector care va fi pe pozitia index dupa ce vectorul este sortat. 
+
+namespace SortedIndex;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Vector = ");
+        int[] v = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+        int n = v.Length;
+
+        Console.Write("Index = ");
+        int index = int.Parse(Console.ReadLine()!);
+        if (index < 0 || index >= n)
+        {
+            Console.WriteLine("index is invalid");
+            return;
+        }
+
+
+        //bubble sort
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n - i - 1; ++j)
+            {
+                if (v[j] > v[j + 1])
+                    (v[j], v[j + 1]) = (v[j + 1], v[j]);
+            }
+        }
+
+        Console.WriteLine($"Index of the sorted vector is {v[index]}");
+    }
+}
