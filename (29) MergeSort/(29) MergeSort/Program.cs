4de8556749f361e835279a257f170a7916acdbe9
FelixOrban <felixorban123@gmail.com>
2025-11-22 11:15:49 +0200

    a

---
+++ (29) MergeSort/(29) MergeSort/Program.cs
@@ -0,0 +1,54 @@
+﻿///MergeSort. Sortati un vector folosind metoda MergeSort.
+
+namespace MergeSort;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Vector = ");
+        int[] v = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+        int n = v.Length;
+
+        MergeSplit(v, n);
+        Console.WriteLine(string.Join(" ", v));
+    }
+
+    static void MergeSplit(int[] v, int n)
+    {
+        if (n <= 1) //splitting the vector until we have n number vectors, than merge in pairs and so on.
+            return;
+
+        int mid = n / 2;
+
+        int[] left = v.Take(mid).ToArray();
+        int[] right = v.Skip(mid).ToArray();
+
+        MergeSplit(left, left.Length);
+        
+        MergeSplit(right, right.Length);
+
+        MergeSort(v, left, right);
+    }
+
+    static void MergeSort(int[] v, int[] left, int[] right)
+    {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.Length && j < right.Length)
+        {
+            if (left[i] <= right[j])
+                v[k++] = left[i++];
+            else
+                v[k++] = right[j++];
+        }
+
+        while (i < left.Length)
+            v[k++] = left[i++];
+
+        while (j < right.Length)
+            v[k++] = right[j++];
+    }
+}
